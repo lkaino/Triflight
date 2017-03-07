05e58db90370152e8e00ad52351a51906691c9ef
Jaakko Laurikainen <lkaino@gmail.com>
2017-03-07 21:03:02 +0200

    Ditched the servo saturation based on distance from setpoint
    Implemented similar I term handling as before on Triflight.
    
    LOS testing shows it's working well.

--- src/main/flight/mixer.c
+++ src/main/flight/mixer.c
@@ -283,10 +283,9 @@ float getMotorMixRange()
 
 bool mixerIsOutputSaturated(int axis, float errorRate)
 {
-    UNUSED(errorRate);
     if (axis == FD_YAW && triMixerInUse()) {
 
-        return triIsServoSaturated();
+        return triIsServoSaturated(errorRate);
     } else {
         return motorMixRange >= 1.0f;
     }

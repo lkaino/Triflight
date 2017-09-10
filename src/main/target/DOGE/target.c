ca5d4dffe8560668dac0a6b567480134846eb4e4
Jaakko Laurikainen <lkaino@gmail.com>
2017-09-10 10:50:36 +0300

    Servo feedback for DOGE
    DOGE target now uses LED strip pin for RSSI servo feedback.
    
    LED strip is disabled for DOGE target.
    
    Thanks @jihlein and @litterbug.

--- src/main/target/DOGE/target.c
+++ src/main/target/DOGE/target.c
@@ -34,5 +34,5 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM2,  CH2, PA1,  TIM_USE_MOTOR, 1), // PWM7 - PA1
     DEF_TIM(TIM3,  CH3, PB0,  TIM_USE_MOTOR, 1), // PWM8 - PB1
     DEF_TIM(TIM3,  CH4, PB1,  TIM_USE_MOTOR, 1), // PWM9 - PB0
-    DEF_TIM(TIM16, CH1, PA6,  TIM_USE_LED,   1), // PWM9 - PB0
+    //DEF_TIM(TIM16, CH1, PA6,  TIM_USE_LED,   1), // PWM9 - PB0
 };

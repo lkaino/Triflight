17fa32162694bed6e70494c87302fd368e4888ff
Jaakko Laurikainen <lkaino@gmail.com>
2017-10-21 12:11:19 +0300

    Version to 0.7 Beta 3

--- src/main/build/version.h
+++ src/main/build/version.h
@@ -15,7 +15,7 @@
  * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define FC_FIRMWARE_NAME            "Triflight 0.7 Beta 2 - new control test v8"
+#define FC_FIRMWARE_NAME            "Triflight 0.7 Beta 3"
 #define FC_VERSION_MAJOR            3  // increment when a major release is made (big new feature, etc)
 #define FC_VERSION_MINOR            1  // increment when a minor release is made (small new feature, change etc)
 #define FC_VERSION_PATCH_LEVEL      7  // increment when a bug is fixed
